9455ba7c9aab54a1607017001a2fcc8ba717e313
Ruto <ruto@protonmail.ch>
2021-02-18 16:01:22 -0500

    Update Theme Toggle

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -15,13 +15,13 @@
       <div
         :class="{ '-translate-y-48 sm:translate-y-0': collapse }"
         class="flex flex-col items-center sm:flex-row space-y-3 sm:space-y-0 sm:space-x-6 text-white px-4 transform transition-all ease-in-out duration-200 bg-gray-600 py-6 sm:py-4 z-40">
-        <ThemeSwitch />
         <a class="text-lg transition-colors hover:text-purple-200 focus:text-purple-400" href="#AboutMe" @click="collapse = true">About Me</a>
         <a class="text-lg transition-colors hover:text-purple-200 focus:text-purple-400" href="#Projects" @click="collapse = true">Projects</a>
         <a class="text-lg transition-colors hover:text-purple-200 focus:text-purple-400" href="#Contact" @click="collapse = true">Contact</a>
         <a class="transition-colors hover:text-purple-200 focus:text-purple-400" href="https://github.com/devruto">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20"><path fill="currentColor" fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path></svg>
         </a>
+        <ThemeSwitch class="pt-4 sm:pt-0" />
       </div>
     </div>
   </div>
--- src/components/ThemeSwitch.vue
+++ src/components/ThemeSwitch.vue
@@ -1,10 +1,16 @@
 <template>
-  <div class="flex h-2 w-20 outline-none">
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <div class="flex h-2 w-20 outline-none items-center" @click="onToggle">
+    <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
     </svg>
-    <button @click="onToggle">Switch</button>
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <div class="relative px-1">
+      <div class="bg-gray-200 w-10 h-5 rounded-full"></div>
+      <div
+        class="absolute w-4 h-4 bg-gray-800 rounded-full inset-y-0.5 left-2 transform transition-all ease-in-out duration-200"
+        :class="{ 'translate-x-4': dark }"
+      ></div>
+    </div>
+    <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
     </svg>
   </div>
