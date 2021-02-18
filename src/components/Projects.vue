db50402baf8f62b81606b500c7edc955477aba3e
Ruto <ruto@protonmail.ch>
2021-02-18 16:44:30 -0500

    Update Project modal

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,8 +1,24 @@
 <template>
   <div>
-    <div v-if="selectedProject" class="fixed h-full w-full md:h-2/3 md:w-2/3 md:mx-auto md:my-auto z-10 inset-0 pt-20 px-4 pb-4">
-      <div class="bg-blue-400 rounded h-full w-full mx-auto my-auto p-4">
-        modal
+    <div
+      class="fixed h-full w-full md:h-2/3 md:w-2/3 md:mx-auto md:my-auto z-10 inset-0 pt-20 px-4 pb-4 transition transform duration-200 scale-0"
+      :class="{ 'scale-100': selectedProject }"
+      >
+      <div class="bg-gray-900 shadow-lg rounded h-full w-full mx-auto my-auto p-4">
+        <div v-if="selectedProject" class="flex flex-col">
+          <div class="flex flex-row justify-between items-center">
+            <div class="text-3xl font-bold">{{ selectedProject.name }}</div>
+            <button class="w-8 h-8 focus:outline-none" @click="selectedProject = false">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
+          </div>
+          <a class="text-blue-200" target="_blank" :href="selectedProject.link">{{ selectedProject.link }}</a>
+          <div>
+            content
+          </div>
+        </div>
       </div>
     </div>
     <div class="text-4xl font-mono">
@@ -13,7 +29,7 @@
     </div>
     <div class="flex flex-col">
       <div
-        class="border-2 rounded-lg border-gray-200 h-40 w-60 p-2 overflow-hidden"
+        class="border-2 rounded-lg border-gray-200 h-40 w-60 p-2 overflow-hidden cursor-pointer hover:bg-gray-700"
         v-for="project of projects"
         @click="selectProject(project)"
         :key="project.name">
