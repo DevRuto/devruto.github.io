c8a5d9a2a757c29e12e14e1d9b58efc1d599e412
Ruto <ruto@protonmail.ch>
2021-02-22 03:59:56 -0500

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
-  <div id="app" class="text-black dark:text-white transition">
+  <div id="app" class="flex flex-col h-screen overflow-y-auto text-black dark:text-white transition">
     <Navbar />
-    <div class="container flex flex-col space-y-12 mx-auto pt-6 p-4 z-0">
+    <div class="container flex-grow flex flex-col space-y-12 mx-auto pt-6 p-4 z-0">
       <AboutMe id="AboutMe" />
       <Projects id="Projects" />
       <Contact id="Contact" />
     </div>
+    <Footer />
   </div>
 </template>
 
@@ -14,6 +15,7 @@ import Navbar from './components/Navbar.vue';
 import Contact from './components/Contact.vue';
 import Projects from './components/Projects.vue';
 import AboutMe from './components/AboutMe.vue';
+import Footer from './components/Footer.vue';
 
 export default {
   name: 'App',
@@ -22,6 +24,7 @@ export default {
     Contact,
     Projects,
     AboutMe,
+    Footer,
   },
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="bg-gray-700 w-full px-4 py-4">
+    <div class="text-white font-semibold">
+      Winston &copy; 2021
+    </div>
+  </div>
+</template>
