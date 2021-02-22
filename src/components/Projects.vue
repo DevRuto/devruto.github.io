5f5799bdf6fa0b1ce9f08b833108773205157e62
Ruto <ruto@protonmail.ch>
2021-02-22 18:08:57 -0500

    Update Project modal

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <div
-      class="fixed h-2/3 md:h-1/2 w-full md:w-3/5 md:mx-auto my-auto overflow-hidden z-10 inset-0 pt-20 px-4 pb-4 transition transform duration-200 scale-0"
+      class="fixed md:h-3/5 w-full md:w-3/5 md:mx-auto my-auto overflow-hidden z-10 inset-0 pt-20 px-4 pb-4 transition transform duration-200 scale-0"
       :class="{ 'scale-100': selectedProject }"
       >
       <div class="relative bg-gray-100 dark:bg-gray-900 shadow-lg border-2 border-gray-700 overflow-y-auto rounded-lg h-full w-full mx-auto my-auto p-4">
-        <div v-if="selectedProject" class="flex flex-col">
+        <div v-if="selectedProject" class="flex flex-col space-y-3">
           <div class="flex flex-row justify-between items-center">
             <div class="text-3xl font-bold">{{ selectedProject.name }}</div>
             <button class="w-8 h-8 focus:outline-none" @click="selectedProject = false">
@@ -14,15 +14,17 @@
               </svg>
             </button>
           </div>
-          <a class="text-blue-500 hover:underline" target="_blank" :href="selectedProject.link">{{ selectedProject.link }}</a>
-          <div class="flex flex-col sm:flex-row pt-4 justify-center sm:justify-start space-y-4 sm:space-y-0 space-x-0 sm:space-x-4">
-            <div class="flex flex-col border border-gray-700 rounded-lg p-4">
-              <div class="font-semibold mx-auto">Description</div>
-              <hr class="border-gray-600 pb-4" />
-              <div class="">
-                {{ selectedProject.shortDesc }}
-              </div>
+          <div class="flex flex-col sm:flex-row sm:space-x-4 space-y-4 sm:space-y-0 divide-dashed">
+            <div>
+              <div class="font-bold">Description: </div>
+              <div class="inline">{{ selectedProject.shortDesc }}</div>
             </div>
+            <div>
+              <div class="font-bold">Repo: </div>
+              <a class="text-blue-500 hover:underline" target="_blank" :href="selectedProject.link">{{ selectedProject.link }}</a>
+            </div>
+          </div>
+          <div class="flex flex-col sm:flex-row pt-4 justify-center sm:justify-start space-y-4 sm:space-y-0 space-x-0 sm:space-x-4">
             <div class="flex flex-col border border-gray-700 rounded-lg p-4">
               <div class="font-semibold mx-auto">Technologies</div>
               <hr class="border-gray-600 pb-4" />
@@ -31,6 +33,13 @@
                   <a class="text-blue-500 hover:underline" :href="tool.link" target="_blank">{{ tool.name }}</a>
                   - {{ tool.desc }}
                 </div>
+              </div>
+            </div>
+            <div class="flex flex-col border border-gray-700 rounded-lg p-4 w-full sm:w-2/3">
+              <div class="font-semibold mx-auto">Summary</div>
+              <hr class="border-gray-600 pb-4" />
+              <div class="flex flex-col space-y-3">
+                <div v-html="selectedProject.longDesc"></div>
               </div>
             </div>
           </div>
@@ -83,7 +92,22 @@ export default {
           language: 'JavaScript',
           logo: ICON_GITHUB,
           shortDesc: 'My Personal Portfolio',
-          longDesc: '',
+          longDesc: `
+            <p>
+              This is the first portfolio. It was pretty fun to make, as I've never made one before.
+              The most difficult part of the project was figuring out the design and frameworks I wanted to use.
+              Ultimately, I decided to use VueJS, a framework I really enjoyed working with, and TailwindCSS just
+              to have more practice with it.
+            </p>
+            <p class="pt-2">
+              Another difficult part of this project was figuring out what to say in the "About Me" section
+              of the portfolio.
+            </p>
+            <p class="pt-2">
+              For the first revision of this project, it seems decent to me, but definitely could be a lot better.
+              As time goes on, it'll be interesting to see how this portfolio will evolve.
+            </p>
+          `,
           tools: [
             {
               name: 'Vue.js',
